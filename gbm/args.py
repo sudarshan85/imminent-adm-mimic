d371c15ce14283b6e9647f749aff2723b55a74e4
Sudarshan <su0@ornl.gov>
2019-06-13 12:57:07 -0400

    GBM

---
+++ gbm/args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from argparse import Namespace
+from pathlib import Path
+
+path = Path('../data')
+workdir=path/'work_dir/gbm'
+
+args = Namespace(
+  path=path,
+  workdir=workdir,
+  dataset_csv=path/'processed_dataset.csv',
+  cols=['class_label', 'scispacy_note'],
+  modeldir=workdir/'models',
+  min_freq=3,
+  bc_threshold=0.3,
+  start_seed=127,
+  )
